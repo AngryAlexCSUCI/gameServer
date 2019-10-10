4c84fb5072ac53ec26081a69168e9ddba86080e9
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2019-10-09 21:56:15 -0700

    never mind

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let app = require('express')()
 let server = require('http').Server(app)
 let io = require('socket.io')(server)
 
+server.listen(3000)
 
 app.get('/', function (req, res) {
     res.send('test')
