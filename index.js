8725d5a829b8ee24469054ff349caebd35df7467
Sarah Schmidt <sarah.schmidt344@myci.csuci.edu>
2019-10-20 19:11:53 -0700

    add log

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 3000 })
 
 wss.on('connection', function connection(ws) {
+    console.log("Connected")
     ws.on('message', function incoming(message) {
         console.log('received: %s', message)
     })
